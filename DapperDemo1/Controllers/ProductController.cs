02edf949a5fcad79d6d38003e902f856b9dadb2a
Pranjal Agarwal <INT141@xceedance.com>
2020-07-06 13:18:57 +0530

    initial commit

---
+++ DapperDemo1/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DapperDemo1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public  ProductRepository productRepository;
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return productRepository.GetAll();
+
+        }
+
+        [HttpGet("{id}")]
+        public Product Gets(int id)
+          {
+            return productRepository.GetById(id);
+          }
+
+
+
+            [HttpPost]
+       public void Post([FromBody]Product prod)
+        {
+            if (ModelState.IsValid)
+                productRepository.Add(prod);
+        }
+         [HttpPut("{id}")]
+       public void Put(int id, [FromBody] Product prod)
+       {
+            prod.ProductId = id;
+            if (ModelState.IsValid)
+                productRepository.Update(prod);
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            productRepository.Delete(id);
+
+        }
+    }
+}
---
+++ DapperDemo1/Models/ProductRepository.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo1.Models
+{
+    public class ProductRepository
+    {
+        private string connectionstring;
+        public ProductRepository()
+        {
+            connectionstring = "Server=(localdb)\\MSSQLLocalDB;Database=Pranjal3; Trusted_Connection = True; MultipleActiveResultSets = true";
+        }
+        public  IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionstring);
+            }
+        }
+        public void Add(Product prod)
+        {
+            using(IDbConnection dbconnection=Connection)
+            {
+                string sQuery = @"INSERT INTO Products(Name,Quantity,Price) VALUES(@Name,@Quantity,@Price)";
+                dbconnection.Open();
+                dbconnection.Execute(sQuery, prod);
+            }
+        }
+
+        
+
+        public IEnumerable<Product>GetAll()
+        {
+            using (IDbConnection dbconnection = Connection)
+            {
+                string sQuery = @"Select * FROM Products";
+                dbconnection.Open();
+                return dbconnection.Query<Product>(sQuery);
+            }
+        }
+
+        public Product GetById(int id)
+        {
+            using (IDbConnection dbconnection = Connection)
+            {
+                string sQuery = @"Select * FROM Products Where ProductId=@Id";
+                dbconnection.Open();
+                return dbconnection.Query<Product>(sQuery,new { Id = id }).FirstOrDefault();
+            }
+        }
+
+        public void Delete (int id)
+        {
+            using (IDbConnection dbconnection = Connection)
+            {
+                string sQuery = @"DELETE FROM Products Where ProductId=@Id";
+                dbconnection.Open();
+                dbconnection.Execute(sQuery, new { Id = id });
+            }
+        }
+
+        public void Update(Product prod)
+        {
+            using (IDbConnection dbconnection = Connection)
+            {
+                string sQuery = @"UPDATE Products SET Name=@Name,Quantity=@Quantity,Price=@Price Where ProductId=@ProductId";
+                dbconnection.Open();
+                dbconnection.Query(sQuery, prod);
+            }
+        }
+    }
+}
